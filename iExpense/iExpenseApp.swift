694ab7f4032f257213501c6c911e2b52944cba80
Mark J Perryman <appledad05@gmail.com>
2022-05-21 07:39:35 -0600

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Mark Perryman on 5/21/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
